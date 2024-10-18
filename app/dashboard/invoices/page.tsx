6f1b034dfb0112efe0a3673f33d79313c7aa5abc
yusufuysal <yusufuysal.dev@gmail.com>
2024-10-18 14:50:44 +0300

    Fix build issue about useSearchParams

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,7 @@
-//import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-//import Table from '@/app/ui/invoices/table';
 import { lusitana } from '@/app/ui/fonts';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
-//import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-//import { Suspense } from 'react';
- 
+//import Search from '@/app/ui/search';
+
 export default async function Page() {
   return (
     <div className="w-full">
@@ -13,14 +9,14 @@ export default async function Page() {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
+        {/*<Search placeholder="Search invoices..." />*/}
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      {/*<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>*/}
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        {/*<Pagination totalPages={totalPages} />*/}
       </div>
     </div>
   );
